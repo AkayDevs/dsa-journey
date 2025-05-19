474c513da5a5a6f827d6646a0f22414daf3d97ed
Akay999 <akaygamer199@gmail.com>
2025-05-19 22:07:21 +0530

    Created Fibonacci algorithm optimised

---
+++ Notes/DSA University of San Diego/Algorithmic Toolbox/Week_2_solutions/fibonacci.py
@@ -0,0 +1,18 @@
+def fibonacci_number(n):
+    if n <= 1:
+        return n
+
+    return fibonacci_number(n - 1) + fibonacci_number(n - 2)
+
+def fibonacci_optimised(n):
+    f_list = [0] * n
+    f_list[1] = 1
+    for i in range(2, n):
+        f_list[i] = f_list[i - 1] + f_list[i - 2]
+    return f_list[n - 1]
+    
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    print(fibonacci_optimised(input_n))
